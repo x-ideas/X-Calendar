9c36f4f5f76f9972874273eaf661b50a6072539a
向家祥 <xjiaxiang_86@163.com>
2020-11-16 22:53:51 +0800

    chore(calendar-core): install sass, rollup, assemblyscript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,15 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    "airbnb",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
+  extends: ['airbnb', 'eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint"],
-  rules: {},
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'import/prefer-default-export': 'off',
+  },
 };
