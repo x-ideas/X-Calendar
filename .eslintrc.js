5675f7a6b1e7c3db1f9f4fdddc75845dfcca5c07
向家祥 <xjiaxiang_86@163.com>
2021-05-09 09:51:41 +0800

    feat(eslint): 删除会引起奇怪问题的eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['airbnb', 'eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 
   parser: '@typescript-eslint/parser',
   parserOptions: {
