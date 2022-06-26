46394d12f059f5226a7164f67601e5477a33e1d7
向家祥 <xjiaxiang_86@163.com>
2022-06-26 22:53:11 +0800

    chore: add prettier, compat to eslint
    and modified
    * resolve typescript alias in monorepo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
@@ -8,6 +9,7 @@ module.exports = {
     'airbnb',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:compat/recommended',
     'prettier',
   ],
 
@@ -16,16 +18,28 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
+    /**
+     * 开启prettier
+     */
+    'prettier/prettier': 'error',
     'no-underscore-dangle': 'off',
     'import/prefer-default-export': 'off',
     'import/extensions': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
   },
   settings: {
     'import/resolver': {
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+      typescript: {
+        // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        alwaysTryTypes: true,
+        // use <root>/path/to/folder/tsconfig.json
+        project: ['./tsconfig.json', 'packages/**/tsconfig.json'],
       },
     },
   },
